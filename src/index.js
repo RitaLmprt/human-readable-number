f9cdb604e0b7fa44d86d8e1e4caf19326c332dc2
RitaLmprt <rita.lempert@gmail.com>
2022-01-08 14:19:40 +0200

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,66 @@
 module.exports = function toReadable (number) {
-  let arr = number.toString()
-
-  let human_readable_number = []
-
-  for (let i = arr.length - 1; i >= 0; i--) {
-
-
-    let curr = Number(arr[i])
-
-    let value = ''
-
-    if (curr === 0) { value = 'zero' }
-    if (curr === 1) { value = 'one' }
-    if (curr === 2) { value = 'two' }
-    if (curr === 3) { value = 'three' }
-    if (curr === 4) { value = 'four' }
-    if (curr === 5) { value = 'five' }
-    if (curr === 6) { value = 'six' }
-    if (curr === 7) { value = 'seven' }
-    if (curr === 8) { value = 'eight' }
-    if (curr === 9) { value = 'nine' }
-
-    if (i === arr.length - 1) {
-      if (curr !== 0) {
-        human_readable_number.unshift(value)
-      } 
-      else {
-        arr.length === 1 && human_readable_number.unshift(value)
-      }
-    } 
-    else if (i === arr.length - 2) {
-    if (curr === 1) {
-        human_readable_number.pop()
-        if (Number(arr[arr.length - 1]) === 0) { human_readable_number.unshift('ten')}
-        if (Number(arr[arr.length - 1]) === 1) { human_readable_number.unshift('eleven') }
-        if (Number(arr[arr.length - 1]) === 2) { human_readable_number.unshift('twelve') }
-        if (Number(arr[arr.length - 1]) === 3) { human_readable_number.unshift('thirteen') }
-        if (Number(arr[arr.length - 1]) === 4) { human_readable_number.unshift('fourteen') }
-        if (Number(arr[arr.length - 1]) === 5) { human_readable_number.unshift('fifteen') }
-        if (Number(arr[arr.length - 1]) === 6) { human_readable_number.unshift('sixteen') }
-        if (Number(arr[arr.length - 1]) === 7) { human_readable_number.unshift('seventeen') }
-        if (Number(arr[arr.length - 1]) === 8) { human_readable_number.unshift('eighteen') }
-        if (Number(arr[arr.length - 1]) === 9) { human_readable_number.unshift('nineteen') }
-      }
-
-      if (curr >= 2 && curr <= 9) {
-        if (curr === 2) { human_readable_number.unshift('twenty') }
-        if (curr === 3) { human_readable_number.unshift('thirty') }
-        if (curr === 4) { human_readable_number.unshift('forty') }
-        if (curr === 5) { human_readable_number.unshift('fifty') }
-        if (curr === 6) { human_readable_number.unshift('sixty') }
-        if (curr === 7) { human_readable_number.unshift('seventy') }
-        if (curr === 8) { human_readable_number.unshift('eighty') }
-        if (curr === 9) { human_readable_number.unshift('ninety') }
-      }
-    } 
-    else if (i === arr.length - 3) {
-      human_readable_numberhuman_readable_number.unshift(`${value} hundred`)
+  const words  = {
+    hundred: {
+        100: "one hundred ",
+        200: "two hundred ",
+        300: "three hundred ",
+        400: "four hundred ",
+        500: "five hundred ",
+        600: "six hundred ",
+        700: "seven hundred ",
+        800: "eight hundred ",
+        900: "nine hundred "
+    },
+    dec: {
+        10: "ten ",
+        20: "twenty ",
+        30: "thirty ",
+        40: "forty ",
+        50: "fifty ",
+        60: "sixty ",
+        70: "seventy ",
+        80: "eighty ",
+        90: "ninety "
+    },
+    unit: {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"
+    },
+    tenths: {
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen"
     }
   }
 
-  return human_readable_number.join(' ')
-}
+  let unitNum = number % 10;
+  let tenthNum = number % 100;
+  let decNum = 0;
+
+  if(tenthNum > 10 && tenthNum < 20) {
+    decNum = tenthNum;
+    unitNum = '';
+  } else {
+    decNum = (number - unitNum) % 100;
+  }
+
+  let hundredNum = number - decNum - unitNum;
+
+  if(number == 0) return "zero";
+
+  return `${words.hundred[hundredNum] || ''}${((decNum > 10 && decNum < 20) ? words.tenths[decNum] : words.dec[decNum]) || ''}${words.unit[unitNum] || ''}`.trim()
+}   
+
